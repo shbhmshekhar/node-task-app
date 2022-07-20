65ceac98643a6ec6c003084a3e2e1691ba545ce5
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-20 17:16:18 +0530

    learned creating new doc on mongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,14 @@
 const mongodb = require('mongodb');
 
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = mongodb;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectId();
+
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -15,9 +20,37 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Task app',
-      data_type: 'first entry',
-    });
+    // db.collection('users').insertOne(
+    //   {
+    //     // _id: id,
+    //     name: 'Task app',
+    //     data_type: 'First entry',
+    //   },
+    //   (err, res) => {
+    //     if (error) {
+    //       return console.log('Unable to insert User');
+    //     }
+    //     console.log(res.insertedId);
+    //   }
+    // );
+    // INsert Multiple document
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Task app - 2',
+    //       data_type: 'Third entry',
+    //     },
+    //     {
+    //       name: 'Task app - 3',
+    //       data_type: 'Fourth entry',
+    //     },
+    //   ],
+    //   (err, res) => {
+    //     if (error) {
+    //       return console.log('Unable to insert User');
+    //     }
+    //     console.log(res);
+    //   }
+    // );
   }
 );
