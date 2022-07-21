0d4c6a8bfb10c95eefe9866da5fb4ec5bc3ae5a0
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-21 14:17:00 +0530

    Create a new user using mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,5 +2,22 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
-  useCreateINdex: true,
+  //   useCreateIndex: true,
 });
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+// CREATE A NEW USER
+
+const me = new User({ name: 'Shubham', age: 27 });
+
+me.save()
+  .then((res) => console.log('data saved', re))
+  .catch((err) => console.log('something went wrong', err));
