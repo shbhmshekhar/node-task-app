6a085d9e0d1e238d3b8d131b0126a41f5f8eb206
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-21 13:46:44 +0530

    learnt to update document using updateOne

--- mongodb.js
+++ mongodb.js
@@ -7,8 +7,8 @@ const databaseName = 'task-manager';
 
 const id = new ObjectId();
 
-console.log(id.id.length);
-console.log(id.toHexString().length);
+// console.log(id.id.length);
+// console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -39,10 +39,12 @@ MongoClient.connect(
     //     {
     //       name: 'Task app - 2',
     //       data_type: 'Third entry',
+    //       count: 1,
     //     },
     //     {
     //       name: 'Task app - 3',
     //       data_type: 'Fourth entry',
+    //       count: 1,
     //     },
     //   ],
     //   (err, res) => {
@@ -52,5 +54,38 @@ MongoClient.connect(
     //     console.log(res);
     //   }
     // );
+    // db.collection('users').findOne(
+    //   { name: 'Task app', data_type: '123' },
+    //   (err, res) => {
+    //     if (error) {
+    //       return console.log('Unable to find data');
+    //     }
+    //     console.log(res);
+    //   }
+    // );
+
+    // db.collection('users')
+    //   .find({ name: 'Task app' })
+    //   .count((err, res) => {
+    //     console.log(res);
+    //   });
+
+    //Update document
+    db.collection('users')
+      .updateOne(
+        {
+          _id: new ObjectId('62d90b04a37ad2b46e2d53bf'),
+        },
+        {
+          // $set: {
+          //   name: 'new Task name',
+          // },
+          $inc: {
+            count: 1,
+          },
+        }
+      )
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
   }
 );
