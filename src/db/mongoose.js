3c813c6f14af7d5fc915512ffc964c97b910426d
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-21 14:21:26 +0530

    Create a new task using mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,19 +5,37 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   //   useCreateIndex: true,
 });
 
-const User = mongoose.model('User', {
-  name: {
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// // CREATE A NEW USER
+
+// const me = new User({ name: 'Shubham', age: 27 });
+
+// me.save()
+//   .then((res) => console.log('data saved', re))
+//   .catch((err) => console.log('something went wrong', err));
+
+const Tasks = mongoose.model('Tasks', {
+  description: {
     type: String,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
   },
 });
 
-// CREATE A NEW USER
+// CREATE A NEW TASK
 
-const me = new User({ name: 'Shubham', age: 27 });
+const firstTask = new Tasks({ description: 'First Task', completed: true });
 
-me.save()
-  .then((res) => console.log('data saved', re))
+firstTask
+  .save()
+  .then((res) => console.log('data saved', res))
   .catch((err) => console.log('something went wrong', err));
