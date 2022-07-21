c9a4ca2ad2edd4b4c23d994114fd3a6991711d67
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-21 15:55:25 +0530

    added data validation and sanitization to models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -8,32 +9,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // const User = mongoose.model('User', {
 //   name: {
 //     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error('Invalid Email');
+//       }
+//     },
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minLength: 7,
+//     validate(value) {
+//       if (value.toLowerCase().includes('password')) {
+//         throw new Error(`Password can't be ${value}`);
+//       }
+//     },
 //   },
 //   age: {
 //     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("Age can't be negative");
+//       }
+//     },
 //   },
 // });
 
 // // CREATE A NEW USER
 
-// const me = new User({ name: 'Shubham', age: 27 });
+// const secondUser = new User({
+//   name: '   Test User2  ',
+//   email: '   TEST2@test.COM   ',
+//   password: 'pard1',
+// });
 
-// me.save()
-//   .then((res) => console.log('data saved', re))
+// secondUser
+//   .save()
+//   .then((res) => console.log('data saved', res))
 //   .catch((err) => console.log('something went wrong', err));
 
-const Tasks = mongoose.model('Tasks', {
+const Tasks = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
 // CREATE A NEW TASK
 
-const firstTask = new Tasks({ description: 'First Task', completed: true });
+const firstTask = new Tasks({});
 
 firstTask
   .save()
