b224ffbb2e6afee791b5d2c15877001208667de4
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-22 17:52:29 +0530

    Reading from endpoint

--- src/index.js
+++ src/index.js
@@ -14,10 +14,35 @@ app.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.send(user);
+    res.status(201).send(user);
   } catch (err) {
     res.status(400).send(err);
     console.log('Error while saving');
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      return res.status(404).send();
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ err: 'Something Went wrong!' });
   }
 });
 
@@ -25,7 +50,7 @@ app.post('/addtask', async (req, res) => {
   const task = new Task(req.body);
   try {
     await task.save();
-    res.send(task);
+    res.status(201).send(task);
   } catch (err) {
     res.status(400).send(err);
   }
