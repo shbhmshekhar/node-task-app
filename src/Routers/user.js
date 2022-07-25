daefa82aacc2eeab5974434b4f2492e5703e248b
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-26 00:21:26 +0530

    added auth for all user routes

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -56,20 +56,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(500).send({ err: 'Something Went wrong!' });
-  }
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every((update) =>
@@ -79,31 +66,45 @@ router.patch('/users/:id', async (req, res) => {
     return res.status(400).send({ error: 'Invalid Updates' });
   }
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
+    // const user = await User.findById(req.user._id);
+    // if (!user) {
+    //   return res.status(404).send();
+    // }
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
     // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
     //   new: true,
     //   runValidators: true,
     // });
 
-    res.send(user);
+    res.send(req.user);
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-router.delete('/user/:id', async (req, res) => {
+router.delete('/user/me', auth, async (req, res) => {
   try {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
-    if (!deletedUser) return res.status(404).send();
-    res.send(deletedUser);
+    // const deletedUser = await User.findByIdAndDelete(req.user._id);
+    // if (!deletedUser) return res.status(404).send();
+    await req.user.remove();
+    res.send(req.user);
   } catch (err) {
     re.status(500).send();
   }
 });
+
+// router.get('/users/:id', async (req, res) => {
+//   try {
+//     const _id = req.params.id;
+//     const user = await User.findById(_id);
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+//     res.send(user);
+//   } catch (err) {
+//     res.status(500).send({ err: 'Something Went wrong!' });
+//   }
+// });
 
 module.exports = router;
