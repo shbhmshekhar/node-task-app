160c56c7b3c719fb5b8b25bf8a7b8c967dc6db80
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-25 16:01:42 +0530

    added functionality to update user and task

--- src/index.js
+++ src/index.js
@@ -46,6 +46,29 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid Updates' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 app.post('/addtask', async (req, res) => {
   const task = new Task(req.body);
   try {
@@ -80,6 +103,31 @@ app.get('/tasks/:id', async (req, res) => {
     res.status(500).send();
   }
 });
+
+app.patch('/task/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedFieldsToUpdate = ['description', 'completed'];
+  const isUpdateValid = updates.every((update) =>
+    allowedFieldsToUpdate.includes(update)
+  );
+  if (!isUpdateValid) {
+    return res.status(400).send({ error: 'Invalid Updates' });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.delete('/user');
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
